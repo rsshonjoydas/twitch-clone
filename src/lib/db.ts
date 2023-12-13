5c78ece01b95637ad51ac46d6b1a33eeebe7891a
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-12-13 08:38:13 +0600

    ci: database

---
+++ src/lib/db.ts
@@ -0,0 +1,12 @@
+/* eslint-disable vars-on-top */
+/* eslint-disable no-var */
+/* eslint-disable no-unused-vars */
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
