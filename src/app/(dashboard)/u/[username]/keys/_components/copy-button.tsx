480edf6d4daedb177b0651601773003768481ece
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-12-15 18:58:18 +0600

    feat: key settings

---
+++ src/app/(dashboard)/u/[username]/keys/_components/copy-button.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { CheckCheck, Copy } from 'lucide-react';
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+
+interface CopyButtonProps {
+  value?: string;
+}
+
+export const CopyButton = ({ value }: CopyButtonProps) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const onCopy = () => {
+    if (!value) return;
+
+    setIsCopied(true);
+    navigator.clipboard.writeText(value);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
+
+  const Icon = isCopied ? CheckCheck : Copy;
+
+  return (
+    <Button onClick={onCopy} disabled={!value || isCopied} variant='ghost' size='sm'>
+      <Icon className='h-4 w-4' />
+    </Button>
+  );
+};
