dd70827f6c52e9a6eddae6332969d27a5e5f060e
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-12-18 10:04:06 +0600

    feat: search page

---
+++ src/app/(browse)/search/_components/results.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/no-array-index-key */
+import { Skeleton } from '@/components/ui/skeleton';
+import { getSearch } from '@/lib/search-service';
+
+import { ResultCard, ResultCardSkeleton } from './result-card';
+
+interface ResultsProps {
+  term?: string;
+}
+
+export const Results = async ({ term }: ResultsProps) => {
+  const data = await getSearch(term);
+
+  return (
+    <div>
+      <h2 className='mb-4 text-lg font-semibold'>Results for term &quot;{term}&quot;</h2>
+      {data.length === 0 && (
+        <p className='text-sm text-muted-foreground'>
+          No results found. Try searching for something else
+        </p>
+      )}
+      <div className='flex flex-col gap-y-4'>
+        {data.map((result) => (
+          <ResultCard data={result} key={result.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const ResultsSkeleton = () => (
+  <div>
+    <Skeleton className='mb-4 h-8 w-[290px]' />
+    <div className='flex flex-col gap-y-4'>
+      {[...Array(4)].map((_, i) => (
+        <ResultCardSkeleton key={i} />
+      ))}
+    </div>
+  </div>
+);
