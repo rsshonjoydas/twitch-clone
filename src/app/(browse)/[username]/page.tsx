bd2c4ee147d73fe4b9b34980b1987b6ee489ceae
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-12-18 08:27:09 +0600

    feat: username page

--- src/app/(browse)/[username]/page.tsx
+++ src/app/(browse)/[username]/page.tsx
@@ -1,10 +1,9 @@
 import { notFound } from 'next/navigation';
 
+import { StreamPlayer } from '@/components/stream-player';
 import { isBlockedByUser } from '@/lib/block-service';
 import { isFollowingUser } from '@/lib/follow-service';
 import { getUserByUsername } from '@/lib/user-service';
-
-import { Actions } from './_components/actions';
 
 interface UserPageProps {
   params: {
@@ -15,22 +14,18 @@ interface UserPageProps {
 const UserPage = async ({ params }: UserPageProps) => {
   const user = await getUserByUsername(params.username);
 
-  if (!user) {
+  if (!user || !user.stream) {
     notFound();
   }
 
   const isFollowing = await isFollowingUser(user.id);
   const isBlocked = await isBlockedByUser(user.id);
 
-  return (
-    <div className='flex flex-col gap-y-4'>
-      <p>username: {user.username}</p>
-      <p>user ID: {user.id}</p>
-      <p>is following: {`${isFollowing}`}</p>
-      <p>is blocked by this user: {`${isBlocked}`}</p>
-      <Actions userId={user.id} isFollowing={isFollowing} />
-    </div>
-  );
+  if (isBlocked) {
+    notFound();
+  }
+
+  return <StreamPlayer user={user} stream={user.stream} isFollowing={isFollowing} />;
 };
 
 export default UserPage;
